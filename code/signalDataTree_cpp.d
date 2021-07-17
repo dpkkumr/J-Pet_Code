3a0202d2a5f33e2e4f6eb3c1ba9024ab95f10f24
dpkkumr <64909325+dpkkumr@users.noreply.github.com>
2021-07-17 13:47:22 +0200

    Delete signalDataTree_cpp.d

--- code/signalDataTree_cpp.d
+++
@@ -1,7 +0,0 @@
-
-# DO NOT DELETE
-
-signalDataTree_cpp.so: signalDataTree.h /usr/include/root/TFile.h
-signalDataTree_cpp.so: /usr/include/root/TTree.h
-signalDataTree_cpp.so: /usr/include/root/RVersion.h /usr/include/root/RConfig.h /usr/include/root/TClass.h /usr/include/root/TDictAttributeMap.h /usr/include/root/TInterpreter.h /usr/include/root/TROOT.h /usr/include/root/TBuffer.h /usr/include/root/TMemberInspector.h /usr/include/root/TError.h /usr/include/root/RtypesImp.h /usr/include/root/TIsAProxy.h /usr/include/root/TFileMergeInfo.h /usr/include/root/TCollectionProxyInfo.h /usr/bin/rootcling
-signalDataTree_cpp__ROOTBUILDVERSION= 6.22/08
